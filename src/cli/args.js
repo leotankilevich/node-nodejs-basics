30a451d7971878a470357549489729d99410ae2a
Leonid Tankilevich <leonid@republic.co>
2023-06-15 03:42:38 +0400

    CLI module

--- src/cli/args.js
+++ src/cli/args.js
@@ -1,5 +1,13 @@
 const parseArgs = () => {
-    // Write your code here 
+    // Write your code here
+    const result = process.argv.slice(2).reduce((rows, key, index) => (index % 2 == 0 ? rows.push([key])
+        : rows[rows.length - 1].push(key)) && rows, []).reduce((acc, [key, value], i, arr) => {
+        acc += `${key.replace(/-/g, '')} is ${value}${arr.length - 1 === i ? '' : ', '}`
+
+        return acc
+    }, '');
+
+    console.log(result)
 };
 
 parseArgs();
--- src/cli/env.js
+++ src/cli/env.js
@@ -1,5 +1,12 @@
 const parseEnv = () => {
-    // Write your code here 
+    // Write your code here
+    const result = Object.entries(process.env).filter(([key]) => key.startsWith('RSS')).reduce((acc, [key, value]) => {
+        acc += `${key}=${value}; `
+
+        return acc;
+    }, '')
+
+    console.log(result);
 };
 
 parseEnv();
