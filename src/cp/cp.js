8720f3e9ecd2531188ed8017cec3cbec4a6c8c24
Leonid Tankilevich <leonid@republic.co>
2023-06-20 03:14:06 +0400

    tried to solve last task but didn't understand how to do it

--- src/cp/cp.js
+++ src/cp/cp.js
@@ -1,5 +1,22 @@
+import { fork }  from 'node:child_process';
+import {getPath} from "../utils/getPath.js";
+
 const spawnChildProcess = async (args) => {
     // Write your code here
+    const filePath = getPath(import.meta, '/files/script.js')
+    const child = spawn('node', [filePath, '--prop1', 'value1', '--prop2', 'value2'] );
+
+    child.stdout.on('data', (data) => {
+        console.log(`stdout: ${data}`);
+    });
+
+    child.stderr.on('data', (data) => {
+        console.error(`stderr: ${data}`);
+    });
+
+    child.on('close', (code) => {
+        console.log(`child process exited with code ${code}`);
+    });
 };
 
 // Put your arguments in function call to test this functionality
